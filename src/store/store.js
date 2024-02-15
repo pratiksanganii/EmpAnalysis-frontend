541efd9a5cdcb6c9328f176ccc51cc9e47992917
Pratik Sangani <97470054+pratiksanganii@users.noreply.github.com>
2024-02-16 04:32:11 +0530

    redux store setup

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './userSlice';
+
+const store = configureStore({
+  reducer: { user: userReducer },
+});
+
+export default store;
---
+++ src/store/userSlice.js
@@ -0,0 +1,26 @@
+// reducers/userSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    // Define your actions here
+    setData: (state, action) => {
+      state.user = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setData, setLoading, setError } = userSlice.actions;
+export default userSlice.reducer;
