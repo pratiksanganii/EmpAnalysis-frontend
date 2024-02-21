7752221860cbe2ffd281165763beb4d62ed1a628
Pratik Sangani <97470054+pratiksanganii@users.noreply.github.com>
2024-02-21 17:25:49 +0530

    Update Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 export const Layout = () => {
   const user = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     // if having access token navigate to dashboard
-    if (user.user?.accessToken) navigate('/dashboard');
-    else navigate('/login');
-  }, [user, navigate]);
+    if (!['/login', '/signup'].includes(location.pathname))
+      if (user.user?.accessToken) navigate('/dashboard');
+      else navigate('/login');
+  }, [user, navigate, location.pathname]);
   return <Outlet />;
 };
